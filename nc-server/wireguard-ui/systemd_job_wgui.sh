1e821c826c2595093bfe2eb2d0929188728e5151
11tuvork28 <zofi@xamh.de>
2023-01-14 13:19:55 +0100

    added script for systemd service for wgui

---
+++ nc-server/wireguard-ui/systemd_job_wgui.sh
@@ -0,0 +1,26 @@
+cd /etc/systemd/system/
+cat << EOF > wgui.path
+[Unit]
+Description=Watch /etc/wireguard/wg0.conf for changes
+
+[Path]
+PathModified=/etc/wireguard/wg0.conf
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cat << EOF > wgui.service
+[Unit]
+Description=Restart WireGuard
+After=network.target
+
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/systemctl restart wg-quick@wg0.service
+
+[Install]
+RequiredBy=wgui.path
+EOF
+systemctl enable wgui.{path,service}
+systemctl start wgui.{path,service}
