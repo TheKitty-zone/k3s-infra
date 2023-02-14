07507df2a4a2cc27bb3d8926836602ed79dd80e6
11tuvork28 <zofi@xamh.de>
2023-02-14 13:14:31 +0100

    add --chart-search-root=./charts for helm-docs ci

--- .github/helm-docs.sh
+++ .github/helm-docs.sh
@@ -8,5 +8,5 @@ curl --silent --show-error --fail --location --output /tmp/helm-docs.tar.gz http
 tar -xf /tmp/helm-docs.tar.gz helm-docs
 
 # validate docs
-./helm-docs
+./helm-docs --chart-search-root=./charts
 git diff --exit-code
